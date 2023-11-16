d679268f2a88c025d6a2fd22d3cafe941c5d4cec
GuiDDuran <guilherme.d.gea@gmail.com>
2023-11-15 22:19:48 -0300

    Finalizando estilização

--- detalhes.js
+++ detalhes.js
@@ -1,58 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function(){
-
-    const params = new URLSearchParams(window.location.search);
-    const atletaId = params.get('id');
-    const atletaDetalhes = document.getElementById("atleta_detalhes");
-    const atletaImagem = document.getElementById("atleta_imagem");
-    const atletaNome = document.getElementById("atleta_nome");
-    const atletaPosicao = document.getElementById("atleta_posicao");
-    const atletaDescricao = document.getElementById("atleta_descricao");
-    const atletaNomeCompleto = document.getElementById("atleta_nome_completo");
-    const atletaNascimento = document.getElementById("atleta_nascimento");
-    const atletaAltura = document.getElementById("atleta_altura");
-    const voltar = document.getElementById("voltar_pag");
-    voltar.addEventListener("click", () => {
-        window.location.href = `home.html`;
-    });
-
-    function buscaAtleta(id){
-
-        document.getElementById('msg_carregando').style.display = 'block';
-
-        if(id <= 60){
-
-            fetch(`https://botafogo-atletas.mange.li/${id}`)
-            .then(response => {
-            if (!response.ok){
-                throw new Error("Erro")
-            }
-            return response.json()
-            })
-            .then(atleta => {
-                atletaImagem.src = atleta.imagem;
-                atletaNome.textContent = `${atleta.nome}`;
-                atletaPosicao.textContent = `${atleta.posicao}`;
-                atletaDescricao.textContent = `${atleta.descricao}`;
-                atletaNomeCompleto.textContent = `NOME COMPLETO: ${atleta.nome_completo}`;
-                atletaNascimento.textContent = `NASCIMENTO: ${atleta.nascimento}`; 
-                atletaAltura.textContent = `ALTURA: ${atleta.altura}`;
-                document.getElementById('msg_carregando').style.display = 'none';
-            })
-        }
-
-        else{
-            atletaDetalhes.innerHTML = "<p>Atleta não encontrado(a)!!!</p>";
-            atletaDetalhes.style.fontFamily = 'Alumni Sans Inline One, sans-serif';
-            atletaDetalhes.style.fontSize = '50px'
-
-            document.getElementById('msg_carregando').style.display = 'none';
-            return;
-        }
-    }
-    buscaAtleta(atletaId);
-})*/
-
-
 document.addEventListener("DOMContentLoaded", function(){
 
     const params = new URLSearchParams(window.location.search);
@@ -74,8 +19,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
     if (!autenticado || autenticado !== 'true') {
 
-        const msgAcessoNegado = document.getElementById("msgAcessoNegado");
-        msgAcessoNegado.style.display = 'block';
+        const msgAutorizacao = document.getElementById("msg_autorizacao");
+        msgAutorizacao.style.display = 'block';
 
         const atletaDetalhes = document.getElementById("atleta_detalhes");
         atletaDetalhes.style.display = 'none';
--- home.js
+++ home.js
@@ -1,62 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-
-    const logout = document.getElementById("logout");
-    logout.addEventListener("click", () => {
-        window.location.href = `index.html`;
-    });
-
-    const btnMasc = document.getElementById("btn_masc");
-    const btnFem = document.getElementById("btn_fem");
-    const btnAll = document.getElementById("btn_all");
-    btnMasc.addEventListener("click", () => carregaAtletas("masculino"));
-    btnFem.addEventListener("click", () => carregaAtletas("feminino"));
-    btnAll.addEventListener("click", () => carregaAtletas("all"));
-
-
-    const selectEscolhas = document.getElementById("select_escolhas");
-    selectEscolhas.addEventListener("change", function () {
-        const generoSelecionado = this.value;
-        carregaAtletas(generoSelecionado);
-    });
-
-
-function carregaAtletas(genero){
-    const containerElenco = document.getElementById("elenco");
-    containerElenco.innerHTML = '';
-    document.getElementById('msg_carregando').style.display = 'block';
-
-    fetch(`https://botafogo-atletas.mange.li/${genero}`)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(atleta => {
-            const atletaCartao = document.createElement('div');
-            atletaCartao.className = 'atleta-cartao';
-            atletaCartao.addEventListener('click', () => {
-                document.getElementById('msg_carregando').style.display = 'block';
-                window.location.href = `detalhes.html?id=${atleta.id}`;
-           });
-
-            const ateltaImagem = document.createElement('img');
-            ateltaImagem.className = 'atleta-img';
-            ateltaImagem.src = atleta.imagem;
-
-            const atletaDetalhes = document.createElement('div');
-            atletaDetalhes.className = 'atleta-detalhes';
-            atletaDetalhes.innerHTML = ` 
-                <p class="nome-atleta">${atleta.nome}</p> 
-                <p class="saiba-mais">Saiba mais</p>
-            `;
-
-            atletaCartao.appendChild(ateltaImagem);
-            atletaCartao.appendChild(atletaDetalhes);
-            containerElenco.appendChild(atletaCartao);
-        });
-        document.getElementById('msg_carregando').style.display = 'none';
-    })
- 
-    
-}});*/
-
 document.addEventListener("DOMContentLoaded", function () {
 
     const autenticado = localStorage.getItem('autenticado');
@@ -66,10 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
         const containerElenco = document.getElementById("elenco");
         containerElenco.innerHTML = '';
 
-        const msgAcessoNegado = document.getElementById("msgAcessoNegado");
-        msgAcessoNegado.style.display = 'block';
-        
-        containerElenco.appendChild(msgAcessoNegado);
+        const msgAutorizacao = document.getElementById("msg_autorizacao");
+        msgAutorizacao.style.display = 'block';
+
+        containerElenco.appendChild(msgAutorizacao);
 
         const barraEscolhas = document.getElementById("barra-escolhas");
         barraEscolhas.style.display = 'none';
